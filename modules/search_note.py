869819d1ef3df052907bd0f879b3b1ce2b0af066
nathandrake3652 <131317866+nathandrake3652@users.noreply.github.com>
2025-09-24 12:52:26 -0300

    Funcion search_note
    Se implemento una primera version de la funcion search_note la cual permite buscar notas por nombre

--- modules/search_note.py
+++ modules/search_note.py
@@ -1,6 +1,15 @@
 from modules.data_manager import get_notes
 
-def search_note(name){
+def search_note(name):
     notes = get_notes()
-    
-}
+    for note in notes:
+        if note["name"] == name:
+            print("Nota encontrada:")
+            print(f"Título: {note['name']}")
+            print(f"Autor: {note['author']}")
+            print(f"Contenido: {note['content']}")
+            print(f"Fecha: {note['date']}")
+            print(f"Etiquetas: {', '.join(note['tags'])}")
+            return
+
+    print("Nota no encontrada.")
