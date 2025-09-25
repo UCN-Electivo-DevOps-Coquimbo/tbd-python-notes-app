d0969f35a4341ec53c07ae74fbfe734dcb7e9598
Fernando-Aranda <fernando.aranda@alumnos.ucn.cl>
2025-09-25 12:52:34 -0300

    fix(note): add error handling for non-integer note_id in delete_note function

--- modules/delete_note.py
+++ modules/delete_note.py
@@ -1,8 +1,12 @@
 from modules.data_manager import get_notes, save_notes
 
 def delete_note(note_id):
+    try:
+        note_id = int(note_id)  # Asegurarse de que note_id sea un entero
+    except ValueError:
+        return False  # Si no se puede convertir, retornar False
+
     notes = get_notes()
-    note_id = int(note_id)
     nota_por_borrar = [note for note in notes if note["id"] != note_id]
     if len(nota_por_borrar) < len(notes):
         save_notes(nota_por_borrar)
